07c5800b1479cdf15f0c2224a319011feb73c228
Ice Hole <icezgan21@gmail.com>
2023-09-23 14:50:43 +0300

    updadte

--- include/rk.h
+++ include/rk.h
@@ -3,7 +3,35 @@
 #include <cmath>
 
 
-double f0(double x, double u) {
+std::vector<double> operator+(const std::vector<double>& v1, const std::vector<double>& v2) {
+    if (v1.size() != v2.size()) {
+        throw std::runtime_error("Vector sizes do not match for addition.");
+    }
+
+    std::vector<double> result(v1.size());
+    for (size_t i = 0; i < v1.size(); i++) {
+        result[i] = v1[i] + v2[i];
+    }
+    return result;
+}
+
+std::vector<double> operator+(const std::vector<double>& v1, const double& r) {
+    std::vector<double> result(v1.size());
+    for (size_t i = 0; i < v1.size(); i++) {
+        result[i] = v1[i] + r;
+    }
+    return result;
+}
+
+std::vector<double> operator*(const std::vector<double>& v, double scalar) {
+    std::vector<double> result(v.size());
+    for (size_t i = 0; i < v.size(); i++) {
+        result[i] = v[i] * scalar;
+    }
+    return result;
+}
+
+double f0(double p, double x, double u) {
     return -u / 2.0;
 }
 
@@ -11,62 +39,118 @@ double f0sol(double x, double u_0) {
     return exp(-x / 2 + log(u_0));
 }
 
-double f1(double x, double u) {
+double f1(double p, double x, double u) {
     return u * u / (1.0 + pow(x, 4))  + u - u * u * u * sin(10 * x);
 }
 
-double fv(double x, double u) {
-    return 2 * sin(u);
+std::vector<double> f2(double p, double x, std::vector<double> u) {
+    std::vector<double> result(2);
+    result[0] = u[1];
+    result[1] = -p * sin(u[0]);
+    return result;
 }
 
-double rk(double x, double u, double h, double (*f)(double, double)) {
+double rk(double& p, double& x, double& u, double& h, double (*f)(double, double, double)) {
     double k1, k2, k3, k4;
 
-    k1 = f(x, u);
-    k2 = f(x + h/2, u + h * k1 / 2.0);
-    k3 = f(x + h/2, u + h * k2 / 2.0);
-    k4 = f(x + h, u + h * k3);
+    k1 = f(p, x, u);
+    k2 = f(p, x + h/2, u + h * k1 / 2.0);
+    k3 = f(p, x + h/2, u + h * k2 / 2.0);
+    k4 = f(p, x + h, u + h * k3);
 
     return u + h * (k1 + 2 * k2 + 2 * k3 + k4) / 6.0;
 }
 
-double f2(double x, double u) {
-    return rk(x, u, 0.01, fv);
-}
-
-double rkh(double x, double u, double h, double (*f)(double, double)) {
+double rkh(double& p, double& x, double& u, double& h, double (*f)(double, double, double)) {
     double k1, k2, k3, k4;
     double hh = h / 2;
 
-    k1 = f(x, u);
-    k2 = f(x + hh / 2, u + hh * k1 / 2.0);
-    k3 = f(x + hh / 2, u + hh * k2 / 2.0);
-    k4 = f(x + hh, u + hh * k3);
+    k1 = f(p, x, u);
+    k2 = f(p, x + hh / 2, u + hh * k1 / 2.0);
+    k3 = f(p, x + hh / 2, u + hh * k2 / 2.0);
+    k4 = f(p, x + hh, u + hh * k3);
 
     double u2 = u + h * (k1 + 2 * k2 + 2 * k3 + k4) / 6.0;
     double x2 = x + hh;
 
-    k1 = f(x2, u2);
-    k2 = f(x2 + hh / 2, u2 + hh * k1 / 2.0);
-    k3 = f(x2 + hh / 2, u2 + hh * k2 / 2.0);
-    k4 = f(x2 + hh, u2 + hh * k3);
+    k1 = f(p, x2, u2);
+    k2 = f(p, x2 + hh / 2, u2 + hh * k1 / 2.0);
+    k3 = f(p, x2 + hh / 2, u2 + hh * k2 / 2.0);
+    k4 = f(p, x2 + hh, u2 + hh * k3);
 
     return u2 + hh * (k1 + 2 * k2 + 2 * k3 + k4) / 6.0;
 }
 
-double rkv(double x, double u, double& h, double& ele, double epsilon, int& C1, int& C2, double (*f)(double, double)) {
-    double u1 = rk(x, u, h, f);
-    double u2 = rkh(x, u, h, f);
-    ele = abs(u1 - u2) / 15;
+double rkv(double& p, double& x, double& u, double& h, double& ele, double& epsilon, int& C1, int& C2, double (*f)(double, double, double)) {
+    double u1 = rk(p, x, u, h, f);
+    double u2 = rkh(p, x, u, h, f);
+    ele = (u2 - u1) / 15;
+    if (abs(ele) > epsilon) {
+        h /= 2;
+        C1++;
+        return rkv(p, x, u, h, ele, epsilon, C1, C2, f);
+    }
+    else if (abs(ele) < epsilon / 32) {
+        if (h < 0.01) {
+            h *= 2;
+            C2++;
+        }
+        return u1 + ele * 16;
+    }
+    else return u1 + ele * 16;
+} 
+
+std::vector<double> rk(double& p, double& x, std::vector<double>& u, double& h, std::vector<double> (*f)(double, double, std::vector<double>)) {
+    std::vector<double> k1, k2, k3, k4;
+    double hh = h / 2;
+
+    k1 = f(p, x, u);
+    k2 = f(p, x + h / 2, u + k1 * (h / 2.0));
+    k3 = f(p, x + h / 2, u + k2 * (h / 2.0));
+    k4 = f(p, x + h, u + k3 * h);
+
+    return u + (k1 + k2 * 2 + k3 * 2 + k4) * (h / 6.0); 
+}
+
+std::vector<double> rkh(double& p, double& x, std::vector<double>& u, double& h, std::vector<double>(*f)(double, double, std::vector<double>)) {
+    std::vector<double> k1, k2, k3, k4;
+    double hh = h / 2;
+    k1 = f(p, x, u);
+    k2 = f(p, x + hh / 2, u + k1 * (hh / 2.0));
+    k3 = f(p, x + hh / 2, u + k2 * (hh / 2.0));
+    k4 = f(p, x + hh, u + k3 * hh);
+
+    std::vector<double> u2 = u + (k1 + k2 * 2 + k3 * 2 + k4) * (h / 6.0);
+    double x2 = x + hh;
+
+    k1 = f(p, x2, u2);
+    k2 = f(p, x2 + hh / 2, u2 + k1 * (hh / 2.0));
+    k3 = f(p, x2 + hh / 2, u2 + k2 * (hh / 2.0));
+    k4 = f(p, x2 + hh, u2 + k3 * hh);
+
+    return u2 + (k1 + k2 * 2 + k3 * 2 + k4) * (hh / 6.0);
+}
+
+std::vector<double> rkv(double& p, double& x, std::vector<double>& u, double& h, double& ele, double& epsilon, int& C1, int& C2, std::vector<double>(*f)(double, double, std::vector<double>)) {
+    std::vector<double> u1 = rk(p, x, u, h, f);
+    std::vector<double> u2 = rkh(p, x, u, h, f);
+    std::vector<double> S(u.size());
+    ele = 0;
+    for (int i = 0; i < u1.size(); i++) {
+        S[i] = (u2[i] - u1[i]) / 15;
+        if (ele < abs(S[i])) ele = abs(S[i]);
+    }
     if (ele > epsilon) {
         h /= 2;
         C1++;
-        return rkv(x, u, h, ele, epsilon, C1, C2, f);
+        return rkv(p, x, u, h, ele, epsilon, C1, C2, f);
     }
     else if (ele < epsilon / 32) {
-        h *= 2;
-        C2++;
-        return u1;
+        if (h < 0.01) {
+            h *= 2;
+            C2++;
+        }
+        return u1 + (S * 16);
     }
-    else return u1;
+    else return u1 + (S * 16);
 }
--- source/Source.cpp
+++ source/Source.cpp
@@ -1,26 +1,179 @@
-#include "rk.h"
+﻿#include "rk.h"
 #include <iostream>
 #include <vector>
 #include <cmath>
-#include <fstream>
+#include <fstream>  
+#include <sstream>
 
-static double p = 2;
-int main()
-{
-    double a = 0;
-    double b = 10;
-    int Nmax = 10000;
+
+void calculateTask1(double a, double b, double u, int Nmax, double epsilon, double h, std::ofstream& outputFile, std::ofstream& summary, bool conststep) {
     double x = a;
-    double u = 1.0;
-    double u_0 = 1.0;
-    double h = 0.01;
-    double epsilon = 0.01;
+    double u_0 = u;
+    double parameter = 0;
     double u1 = u;
     double u2 = u;
-    double ue = 0;
+    double ue = u;
     double ele = 0;
     int C1 = 0;
     int C2 = 0;
+    double maxele = 0;
+    double maxh = 0;
+    double xmaxh = 0;
+    double minh = 0;
+    double xminh = 0;
+    int i = 0;
+    double maxdev = 0;
+    double xmaxdev = 0;
+    if (conststep == 1) {
+        for (; x <= b - h && i < Nmax; i++) {
+            u1 = rk(parameter, x, u, h, f0);
+            u2 = rkh(parameter, x, u, h, f0);
+            ue = f0sol(x + h, u_0);
+
+            ele = abs(u1 - u2) / 15;
+            if (maxele < ele) { maxele = ele; }
+            if (maxdev < abs(ue - u1)) { maxdev = abs(ue - u1); xmaxdev = x; }
+
+            outputFile << i + 1 << "," << x << "," << u1 << "," << u2 << "," << u1 - u2 << "," << ele << "," << h << "," << C1 << "," << C2 << "," << ue << "," << abs(ue - u1) << "\n";
+            
+            x += h;
+            u = u1;
+        }
+        summary << "n" << "," << "b - xn" << "," << "max|OLP|" << "," << "max|ui-vi|" << "," << "pri x" << "\n";
+        summary << i << "," << b - x << "," << maxele << "," << maxdev << "," << xmaxdev << "\n";
+    }
+    else {
+        for (; x <= b - h && i < Nmax; i++) {
+            u1 = rkv(parameter, x, u, h, ele, epsilon, C1, C2, f0);
+            u2 = rkh(parameter, x, u, h, f0);
+            ue = f0sol(x + h, u_0);
+
+            if (maxele < abs(ele)) { maxele = abs(ele); }
+            if (maxh < h) { maxh = h; xmaxh = x; }
+            if (minh > h) { minh = h; xminh = x; }
+
+            outputFile << i + 1 << "," << x << "," << u1 << "," << u2 << "," << u1 - u2 << "," << ele << "," << h << "," << C1 << "," << C2 << "," << ue << "," << abs(ue - u1) << "\n";
+            x += h;
+            u = u1;
+            if (h < 0.0000001) break;
+        }
+        summary << "n" << "," << "b - xn" << "," << "max|OLP|" << "," << "х2" << "," << "/2" << "," << "max hi" << "," << "pri x" << "," << "min hi" << "," << "pri x" <<  "," << "max|ui-vi|" << "," << "pri x" << "\n";
+        summary << i << "," << b - x << "," << maxele << "," << C1 << "," << C2 << "," << maxh << "," << xmaxh << "," << minh << "," << xminh << "," << maxdev << "," << xmaxdev << "\n";
+    }
+}
+
+void calculateTask2(double a, double b, double u, int Nmax, double epsilon, double h, std::ofstream& outputFile, std::ofstream& summary, bool conststep) {
+    double x = a;
+    double parameter = 3;
+    double u1 = u;
+    double u2 = u;
+    double ele = 0;
+    int C1 = 0;
+    int C2 = 0;
+    double maxele = 0;
+    double maxh = 0;
+    double xmaxh = 0;
+    double minh = 0;
+    double xminh = 0;
+    int i = 0;
+    if (conststep == 1) {
+        for (; x <= b - h && i < Nmax; i++) {
+            u1 = rk(parameter, x, u, h, f1);
+            u2 = rkh(parameter, x, u, h, f1);
+
+            ele = abs(u1 - u2) / 15;
+            if (maxele < ele) { maxele = ele; }
+
+            outputFile << i + 1 << "," << x << "," << u1 << "," << u2 << "," << u1 - u2 << "," << ele << "," << h << "," << C1 << "," << C2 << "\n";
+            x += h;
+            u = u1;
+        }
+        summary << "n" << "," << "b - xn" << "," << "max|OLP|" << "\n";
+        summary << i << "," << b - x << "," << maxele << "\n";
+    }
+    else {
+        for (; x <= b - h && i < Nmax; i++) {
+            u1 = rkv(parameter, x, u, h, ele, epsilon, C1, C2, f1);
+            u2 = rkh(parameter, x, u, h, f1);
+
+            if (maxele < ele) { maxele = ele; }
+            if (maxh < h) { maxh = h; xmaxh = x; }
+            if (minh > h) { minh = h; xminh = x; }
+
+            outputFile << i + 1 << "," << x << "," << u1 << "," << u2 << "," << u1 - u2 << "," << ele << "," << h << "," << C1 << "," << C2 << "\n";
+            x += h;
+            u = u1;
+            if (h < 0.0000001) break;
+        }
+        summary << "n" << "," << "b - xn" << "," << "max|OLP|" << "," << "х2" << "," << "/2" << "," << "max hi" << "," << "pri x" << "," << "min hi" << "," << "pri x" << "\n";
+        summary << i << "," << b - x << "," << maxele << "," << C1 << "," << C2 << "," << maxh << "," << xmaxh << "," << minh << "," << xminh << "\n";
+    }
+}
+
+void calculateTask3(double parameter, double a, double b, double u, double du, int Nmax, double epsilon, double h, std::ofstream& outputFile, std::ofstream& summary, bool conststep) {
+    double x = a;
+    double ele = 0;
+    int C1 = 0;
+    int C2 = 0;
+    double maxele = 0;
+    double maxh = 0;
+    double xmaxh = 0;
+    double minh = 0;
+    double xminh = 0;
+    int i = 0;
+    std::vector<double> uv = { u, du };
+    std::vector<double> uv1;
+    if (conststep == 1) {
+        for (; x <= b - h && i < Nmax; i++) {
+            uv1 = rk(parameter, x, uv, h, f2);
+            std::vector<double> u1d = rkh(parameter, x, uv, h, f2);
+
+            for (int i = 0; i < uv1.size(); i++) {
+                ele = abs(uv1[i] - u1d[i]) / 15;
+                if (maxele < ele) { maxele = ele; }
+            }
+
+            outputFile << i + 1 << "," << x << "," << uv1[0] << "," << uv1[1] << "," << u1d[0] << "," << u1d[1] << "," << uv1[0] - u1d[0] << "," << ele << "," << h << "," << C1 << "," << C2 << "\n";
+            x += h;
+            uv = uv1;
+        }
+        summary << "n" << "," << "b - xn" << "," << "max|OLP|" << "\n";
+        summary << i << "," << b - x << "," << maxele << "\n";
+    }
+    else {
+        for (; x <= b - h && i < Nmax; i++) {
+            uv1 = rkv(parameter, x, uv, h, ele, epsilon, C1, C2, f2);
+
+            if (maxele < ele) { maxele = ele; }
+            if (maxh < h) { maxh = h; xmaxh = x; }
+            if (minh > h) { minh = h; xminh = x; }
+
+            std::vector<double> u1d = rkh(parameter, x, uv, h, f2);
+
+            outputFile << i + 1 << "," << x << "," << uv1[0] << "," << uv1[1] << "," << u1d[0] << "," << u1d[1] << "," << abs(uv1[0] - u1d[0]) << "," << ele << "," << h << "," << C1 << "," << C2 << "\n";
+            x += h;
+            uv = uv1;
+            if (h < 0.0000001) break;
+        }
+        summary << "n" << "," << "b - xn" << "," << "max|OLP|" << ","<< "х2" << "," << "/2" << "," << "max hi" << "," << "pri x" << "," << "min hi" << "," << "pri x" << "\n";
+        summary << i << "," << b - x << "," << maxele << "," << C1 << "," << C2 << ","  << maxh<< "," << xmaxh << "," << minh << "," << xminh << "\n";
+    }
+}
+
+int main()
+{
+    double a, b, epsilon, h, u, du, parameter;
+    int Nmax;
+    int task;
+    bool conststep;
+
+    std::ifstream paramsFile("input_params.txt");
+    if (!paramsFile.is_open()) {
+        std::cerr << "Failed to open input file." << std::endl;
+        return 1;
+    }
+
+    paramsFile >> a >> b >> u >> epsilon >> h >> Nmax >> task >> conststep >> du >> parameter;
 
     std::ofstream outputFile("results.csv");
     if (!outputFile.is_open()) {
@@ -28,53 +181,24 @@ int main()
         return 1;
     }
 
-    for (int i = 0; x <= b && i < Nmax; i++) {
-        u1 = rk(x, u, h, f0);
-        u2 = rkh(x, u, h, f0);
-        ue = f0sol(x + h, u_0);
-        ele = abs(u1 - u2)/15;
-        outputFile << i + 1 << "," << x << "," << u1 << "," << u2 << "," << abs(u1 - u2) << "," << ele << "," << h << "," << C1 << "," << C2 << "," << ue << "," << abs(ue-u1) << "\n";
-        x += h;
-        u = u1;
+    std::ofstream summary("summary.csv");
+    if (!outputFile.is_open()) {
+        std::cerr << "Failed to open output file." << std::endl;
+        return 1;
     }
 
-    x = a;
-    ele = C1 = C2 = 0;
-    u = 1;
-    h = 0.01;
-
-    for (int i = 0; x <= b && i < Nmax; i++) {
-        u1 = rkv(x, u, h, ele, epsilon, C1, C2, f1);
-        u2 = rkh(x, u, h, f1);
-        outputFile << i + 1 << "," << x << "," << u1 << "," << u2 << "," << abs(u1 - u2) << "," << ele << "," << h << "," << C1 << "," << C2 << "\n";
-        x += h;
-        u = u1;
+    if (task == 1) {
+        calculateTask1(a, b, u, Nmax, epsilon, h, outputFile, summary, conststep);
     }
-
-    x = a;
-    ele = C1 = C2 = 0;
-    u = 1;
-    h = 0.01;
-    double v = 1;
-    double v1 = v;
-    double v2 = v;
-    double vepsilon = 0.01;
-    double vele = 0;
-    int vC1 = 0;
-    int vC2 = 0;
-    double vh = 0.01;
-
-    for (int i = 0; x <= b && i < Nmax; i++) {
-        v1 = rkv(x, v, vh, vele, vepsilon, vC1,vC2, fv);
-        v2 = rkh(x, v, vh, fv);
-
-        u1 = rkv(x, u, h, ele, epsilon, C1, C2, f2);
-        u2 = rkh(x, u, h, f2);
-
-        outputFile << i + 1 << "," << x << "," << u1 << "," << u2 << "," << abs(u1 - u2) << "," << ele << "," << h << "," << C1 << "," << C2 << "," << v1 << "," << v2 << "," << abs(v1-v2) << "," << vele << "," << vh << "," << vC1 << "," << vC2 << "," << "\n";
-        x += h;
-        v = v1;
-        u = u1;
+    else if (task == 2) {
+        calculateTask2(a, b, u, Nmax, epsilon, h, outputFile, summary, conststep);
+    }
+    else if (task == 3) {
+        calculateTask3(parameter, a, b, u, du, Nmax, epsilon, h, outputFile, summary, conststep);
+    }
+    else {
+        std::cerr << "Invalid task number." << std::endl;
+        return 1;
     }
 
     outputFile.close();
